713756646bfdb2c5a092a0b66c9dee0b3ec509ed
Parth-11 <parth12.mgg@gmail.com>
2025-06-27 18:20:10 +0530

    Part3 with errors need to still do bmake && bmake install in root and userland

--- userland/testbin/rot13/rot13.c
+++ userland/testbin/rot13/rot13.c
@@ -14,11 +14,35 @@
 char test_string[48] = "The quick brown fox jumps over the lazy dog.";
 
 /* your code here */
+static void test(void){
+	
+	char encoded[48];
+	int length = 44;
+
+	for(int i=0;i<length;i++){
+		int res = encode(ENCODE_ROT13,(int)test_string[i],-1,-1);
+		if(res < 0)
+			_exit(1);
+		encoded[i] = (char)res;
+	}
+
+	encoded[length] = '\0';
+
+	for(int i=0;i<length;i++){
+		if(checksum(CHECKSUM_ROT13,(int)encoded[i])<0)
+			_exit(1);
+	}
+
+	checksum(CHECKSUM_FINISHED,0);
+
+	_exit(0);
+}
 
 int
 main(void)
 {
 	/* and/or here */
+	test();
 
 	return 0;
 }
